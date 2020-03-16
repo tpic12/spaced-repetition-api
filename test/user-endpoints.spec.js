9adc68a81440151200ab41f8818fbd719f527cf3
Taylor Piccarreto <tpic12@gmail.com>
2020-03-16 10:50:25 -0400

    updated tests to spanish

--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -172,17 +172,19 @@ describe('User Endpoints', function () {
           name: 'test name',
         }
         const expectedList = {
-          name: 'French',
+          name: 'Spanish',
           total_score: 0,
           words: [
-            { original: 'entraine toi', translation: 'practice' },
-            { original: 'bonjour', translation: 'hello' },
-            { original: 'maison', translation: 'house' },
-            { original: 'développeur', translation: 'developer' },
-            { original: 'traduire', translation: 'translate' },
-            { original: 'incroyable', translation: 'amazing' },
-            { original: 'chien', translation: 'dog' },
-            { original: 'chat', translation: 'cat' },
+            { original: 'elefante', translation: 'elephant' },
+            { original: 'oso', translation: 'bear' },
+            { original: 'arana', translation: 'spider' },
+            { original: 'caballo', translation: 'horse' },
+            { original: 'pinguino', translation: 'penguin' },
+            { original: 'tortuga', translation: 'turtle' },
+            { original: 'leon', translation: 'lion' },
+            { original: 'ballena', translation: 'whale' },
+            { original: 'tiburon', translation: 'shark' },
+            { original: 'mono', translation: 'monkey' },
           ]
         }
         return supertest(app)
