763a30264d164a9321caecbb7bdb1c463f1f5c1f
Taylor Piccarreto <tpic12@gmail.com>
2020-03-16 10:41:02 -0400

    spanish inserted

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'Spanish', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'elefante', 'elephant', 2),
+  (2, 1, 'oso', 'bear', 3),
+  (3, 1, 'arana', 'spider', 4),
+  (4, 1, 'caballo', 'horse', 5),
+  (5, 1, 'pinguino', 'penguin', 6),
+  (6, 1, 'tortuga', 'turlte', 7),
+  (7, 1, 'leon', 'lion', 8),
+  (8, 1, 'ballena', 'whale', 9)
+  (9, 1, 'tiburon', 'shark', 10),
+  (10, 1, 'mono', 'monkey', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
