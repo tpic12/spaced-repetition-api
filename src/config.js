ba556c277e1c46901e9cd6750c02eef7bf843541
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-19 22:42:06 -0400

    deploy to heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
+  DB_URL: process.env.DB_URL || 'https://spanimal.herokuapp.com/',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
+https://spanimal.herokuapp.com/
+
+
+//'postgresql://dunder-mifflin@localhost/spaced-repetition'
